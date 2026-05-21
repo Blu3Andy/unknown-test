2aef8ab66fea6046b926c15f93b64863f1a02a21
Blu3Andy <software@quins.de>
2026-05-21 18:53:05 +0200

    path change

--- js/quinsAddOns.js
+++ js/quinsAddOns.js
@@ -103,7 +103,7 @@ const clock = new THREE.Clock();
 function renderObject(object, position = new THREE.Vector3(0, 0, 0))
 {
  
-  loader = new GLTFLoader().setPath('/models/addOns/');
+  loader = new GLTFLoader().setPath('../../models/addOns/');
   loader.load(object, (gltf) => {
     console.log('loading model');
     const mesh = gltf.scene;
